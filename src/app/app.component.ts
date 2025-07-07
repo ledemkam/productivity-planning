153a7130039397169746204f1e53ee67ce9b623c
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-07 22:00:41 +0200

    add developemnt and configfura && configuration deploy on script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  //eslint-disable-next-line @typescript-eslint/no-explicit-any
- readonly isStaging = !(environment as any).production;
+
 }
