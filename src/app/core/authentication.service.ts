522a3a4badd0bab32da3db55e991be3bf1d408ec
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-08 06:10:08 +0200

    authentication service register http_post implement

--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,11 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.dev';
+import { Observable } from 'rxjs';
+
+
+
+/**
+ *
+ * @see https://firebase.google.com/docs/reference/rest/auth?hl=de
+ */
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  
+  
+  readonly #http = inject(HttpClient)
+  
+  register(email: string, password: string): Observable<unknown> {
+      const apiKey = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseConfig.apiKey}`;
+      const body = {
+        email,
+        password,
+        returnSecureToken: true
+      };
+      return this.#http.post(apiKey, body);
 
-     // eslint-disable-next-line no-unused-private-class-members
-     readonly #http = inject(HttpClient)
+     }
 }
