4658444cb4330399ff3fa0edeffded98a534646f
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-13 17:47:08 +0200

    feat(Home): create dump component for Home

---
+++ src/app/visitor/home/home-banner/home-banner.dumb.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-banner',
+  imports: [],
+  templateUrl: './home-banner.dumb.component.html',
+  styleUrl: './home-banner.dumb.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeBannerDumbComponent {
+
+}
