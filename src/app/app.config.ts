7b29fd2014b228c94527ba3c75668a4acbc54d5c
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-14 23:36:56 +0200

    test(cardlist): testing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-//import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 
 
@@ -14,6 +13,5 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    // provideHttpClientTesting()
     ]
 };
