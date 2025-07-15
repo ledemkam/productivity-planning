60773eeb5d10e4370ad996f0f04947422bcb89d5
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-15 07:03:19 +0200

    comp(signup): create signup component

---
+++ src/app/visitor/signup/signup.page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  imports: [],
+  templateUrl: './signup.page.component.html',
+  styleUrl: './signup.page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SignupPageComponent {
+
+}
