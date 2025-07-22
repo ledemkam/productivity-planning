acc7d9f9054f4ebc497fc0fd44d5cf23fd66c9c2
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-22 22:31:09 +0200

    src(app): create login view component

---
+++ src/app/visitor/login/login.page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.page.component.html',
+  styleUrl: './login.page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginPageComponent {
+
+}
