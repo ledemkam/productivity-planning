41e8e94e1e9398140c20db5fdc87fe816f84d0c9
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-24 21:24:42 +0200

    src(core): import env korrektur

--- src/app/core/port/user-firebase.service.ts
+++ src/app/core/port/user-firebase.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { UserService } from './user.service';
 import { ignoreElements, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '@env/environment';
 import { User } from '../entity/user.interface';
 
 @Injectable()
