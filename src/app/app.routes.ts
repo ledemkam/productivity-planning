ae62438b15701cbd2ccb638257c68997193de50e
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-24 16:11:49 +0200

    comp(login): implement html template  for  Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './visitor/home/home.page.component';
 import { SignupPageComponent } from './visitor/signup/signup.page.component';
+import { LoginPageComponent } from './visitor/login/login.page.component';
 
 export const routes: Routes = [
   {
     path: 'signup',
     component: SignupPageComponent,
     title: 'Sign Up',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    title: 'Log In',
   },
   {
     path: '',
--- src/app/visitor/login/login.page.component.ts
+++ src/app/visitor/login/login.page.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.page.component.html',
   styleUrl: './login.page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginPageComponent {
+
+  readonly email = signal('');
+  readonly password = signal('');
 
 }
