fb1a0c2fbb37b17839080db199fffa500ed5b04e
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-24 07:28:53 +0200

    test(component): test strategy with jest config

--- src/app/visitor/login/login.page.component.spec.ts
+++ src/app/visitor/login/login.page.component.spec.ts
@@ -20,4 +20,55 @@ describe('LoginPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  describe('when page loading',() => {
+    it.todo('shoul display email field')
+    it.todo('should display password field')
+  })
+
+  describe('when user interact with email field', () => {
+    it.todo('should display error message when field is empty');
+    // () => {
+    //   email.nativeElement.value = '';
+    //   email.nativeElement.dispatchEvent(new Event('input'));
+    //   fixture.detectChanges();
+
+    //   const error = fixture.debugElement.query(
+    //     By.css('[data-testid="error-email-required"]'),
+    //   );
+    //   const errorMessage = error.nativeElement.textContent;
+
+    //   expect(errorMessage).toContain('Email is required.');
+
+    it.todo('should display error message when field do not contain a valid HTML5 email');
+      //() => {
+      // email.nativeElement.value = 'invalid-email';
+      // email.nativeElement.dispatchEvent(new Event('input'));
+      // fixture.detectChanges();
+
+      // const error = fixture.debugElement.query(
+      //   By.css('[data-testid="error-email-pattern"]'),
+      // );
+      // const errorMessage = error.nativeElement.textContent;
+
+      // expect(errorMessage).toContain('Email must be valid.');
+    });
+
+
+  describe('when user interact with password field', () => {
+    it.todo('should display error message when field is empty')
+    // () => {
+      // password.nativeElement.value = '';
+      // password.nativeElement.dispatchEvent(new Event('input'));
+      // fixture.detectChanges();
+
+      // const error = fixture.debugElement.query(
+      //   By.css('[data-testid="error-password-required"]'),
+      // );
+      // const errorMessage = error.nativeElement.textContent;
+
+      // expect(errorMessage).toContain('Password is required.');
+    });
+  });
+
+
