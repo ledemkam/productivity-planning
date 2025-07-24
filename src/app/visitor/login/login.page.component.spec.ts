eee69124405f2214a400d76147fb45f0e77d18f6
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-24 08:11:00 +0200

    test(component): act/arrange/assert implement

--- src/app/visitor/login/login.page.component.spec.ts
+++ src/app/visitor/login/login.page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginPageComponent } from './login.page.component';
+import { By } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -22,53 +23,78 @@ describe('LoginPageComponent', () => {
   });
 
   describe('when page loading',() => {
-    it.todo('shoul display email field')
-    it.todo('should display password field')
+    it('should display email field', () => {
+      // Arrange
+      const email = fixture.debugElement.query(By.css('[data-testid="email"]'));
+      // Act
+      // Assert
+      expect(email).toBeTruthy();
+    })
+    it('should display password field',() => {
+      // Arrange
+     const  password = fixture.debugElement.query(By.css('[data-testid="password"]'));
+      // Act
+      // Assert
+      expect(password).toBeTruthy();
+    })
   })
 
   describe('when user interact with email field', () => {
-    it.todo('should display error message when field is empty');
-    // () => {
-    //   email.nativeElement.value = '';
-    //   email.nativeElement.dispatchEvent(new Event('input'));
-    //   fixture.detectChanges();
+    it('should display error message when field is empty',() => {
+      // Arrange
+      const email = fixture.debugElement.query(By.css('[data-testid="email"]'));
 
-    //   const error = fixture.debugElement.query(
-    //     By.css('[data-testid="error-email-required"]'),
-    //   );
-    //   const errorMessage = error.nativeElement.textContent;
 
-    //   expect(errorMessage).toContain('Email is required.');
+      // Act
+      email.nativeElement.value = '';
+      email.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('[data-testid="error-email-required"]'));
+      const errorMessage = error.nativeElement.textContent;
 
-    it.todo('should display error message when field do not contain a valid HTML5 email');
-      //() => {
-      // email.nativeElement.value = 'invalid-email';
-      // email.nativeElement.dispatchEvent(new Event('input'));
-      // fixture.detectChanges();
+      // Assert
+      expect(error).toBeTruthy();
+      expect(errorMessage).toContain('Email is required.');
+    });
+    it('should display error message when field do not contain a valid HTML5 email',() => {
+      // Arrange
+      const email = fixture.debugElement.query(By.css('[data-testid="email"]'));
 
-      // const error = fixture.debugElement.query(
-      //   By.css('[data-testid="error-email-pattern"]'),
-      // );
-      // const errorMessage = error.nativeElement.textContent;
+      // Act
+      email.nativeElement.value = 'invalid-email';
+      email.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
 
-      // expect(errorMessage).toContain('Email must be valid.');
+      const error = fixture.debugElement.query(By.css('[data-testid="error-email-pattern"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      // Assert
+      expect(error).toBeTruthy();
+      expect(errorMessage).toContain('Email must be valid.');
+    });
     });
 
 
   describe('when user interact with password field', () => {
-    it.todo('should display error message when field is empty')
-    // () => {
-      // password.nativeElement.value = '';
-      // password.nativeElement.dispatchEvent(new Event('input'));
-      // fixture.detectChanges();
+    it('should display error message when field is empty', () => {
+      // Arrange
+      const password = fixture.debugElement.query(By.css('[data-testid="password"]'));
 
-      // const error = fixture.debugElement.query(
-      //   By.css('[data-testid="error-password-required"]'),
-      // );
-      // const errorMessage = error.nativeElement.textContent;
+      // Act
+      password.nativeElement.value = '';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
 
-      // expect(errorMessage).toContain('Password is required.');
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-required"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      // Assert
+      expect(error).toBeTruthy();
+      expect(errorMessage).toContain('Password is required.');
     });
   });
+
+
+    });
 
 
