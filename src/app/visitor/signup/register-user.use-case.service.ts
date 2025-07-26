cb825e08c4f1fbae81e98e6f33d08a5bb64d4f44
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-26 11:14:26 +0200

    comp(signup): implment pattern Use case for new user authentication

--- src/app/visitor/signup/register-user.use-case.service.ts
+++ src/app/visitor/signup/register-user.use-case.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { AuthenticationService } from '@app/core/authentication.service';
+import { User, Visitor } from '@app/core/entity/user.interface';
+import { UserService } from '@app/core/port/user.service';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterUserUseCaseService {
 
-  constructor() { }
+  readonly #authenticationService = inject(AuthenticationService);
+  readonly #userService = inject(UserService)
+
+  // 1. authenticate the new user
+  async execute(visitor: Visitor): Promise<User|Error> {
+    const name = visitor.name;
+    const email = visitor.email;
+    const password = visitor.password;
+
+   const authResponse = await firstValueFrom(this.#authenticationService.register( email, password ));
+
+  // add the JWT token and email to local storage(in session storage)
+    const jwtToken = authResponse.jwtToken;
+    const id = authResponse.userId;
+
+    localStorage.setItem('jwtToken', jwtToken);
+    localStorage.setItem('email', email);
+
+    // Create a user object with the necessary details in the database
+    const user: User = {
+      id,
+      name,
+      email
+
+    };
+    // Create the user in the system
+    await firstValueFrom(this.#userService.create(user,jwtToken));
+
+    // Return the created user
+    return user;
+
+  }
 }
