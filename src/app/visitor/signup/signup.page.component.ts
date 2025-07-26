bfa21b7e9a0ccb1fa1e80bbe653430fadbaaefb7
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-07-26 12:27:06 +0200

    comp(signup): implement workflow  registerUsercase

--- src/app/visitor/signup/signup.page.component.ts
+++ src/app/visitor/signup/signup.page.component.ts
@@ -2,10 +2,16 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  inject,
   signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '@app/core/authentication.service';
+import { RegisterUserUseCaseService } from './register-user.use-case.service';
+import { UserStore } from '@app/core/store/user.store';
+import { Visitor } from '@app/core/entity/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -15,6 +21,12 @@ import { FormsModule } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignupPageComponent {
+readonly store = inject(UserStore);
+readonly authenticationService = inject(AuthenticationService);
+readonly #registerUserUseCase = inject(RegisterUserUseCaseService);
+readonly #router = inject(Router);
+
+
   readonly name = signal('');
   readonly email = signal('');
   readonly password = signal('');
@@ -22,4 +34,20 @@ export class SignupPageComponent {
   readonly isPasswordMatch = computed(
     () => this.password() === this.confirmPassword(),
   );
+
+  //workflow user authentication
+ onSubmit() {
+  // 1.infos collection(collecte donnees)
+  const visitor: Visitor = {
+    name: this.name(),
+    email: this.email(),
+    password: this.password(),
+  }
+  // 2. execute the use case(traitement metier)
+  this.#registerUserUseCase.execute(visitor)
+  .then(() => {
+    // 3. redirect to home page after successful registration
+    this.#router.navigate(['/']);
+  });
+ }
 }
