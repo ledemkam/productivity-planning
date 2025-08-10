6202ba2c985d71e100a6a3cacb7ae49ff9bc5b59
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-08-10 12:04:03 +0200

    chore(app): add lazy loading route update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ShellLayoutComponent } from './menbership/core/shell/shell.layout.component';
 
 
 export const routes: Routes = [
@@ -12,34 +13,41 @@ export const routes: Routes = [
     loadComponent: () => import('./visitor/login/login.page.component').then(c => c.LoginPageComponent),
     title: 'Log In',
   },
-   {
-    path: 'app/dashboard',
-    loadComponent: () => import('./menbership/dashboard/dashboard.page.component').then(c => c.DashboardPageComponent),
-    title: 'Dashboard',
-  },
-   {
-    path: 'app/planning',
-    loadComponent: () => import('./menbership/planning/planning.page.component').then(c => c.PlanningPageComponent),
-    title: 'Planning',
-  },
-   {
-    path: 'app/profil',
-    loadComponent: () => import('./menbership/profil/profil.page.component').then(c => c.ProfilPageComponent),
-    title: 'Profil',
-  },
-   {
-    path: 'app/settings',
-    loadComponent: () => import('./menbership/settings/settings.page.component').then(c => c.SettingsPageComponent),
-    title: 'Settings',
-  },
-   {
-    path: 'app/workday',
-    loadComponent: () => import('./menbership/workday/workday.page.component').then(c => c.WorkdayPageComponent),
-    title: 'Workday',
-  },
+  {
+    path: 'app',
+    component: ShellLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./menbership/dashboard/dashboard.page.component').then(c => c.DashboardPageComponent),
+        title: 'Dashboard',
+      },
+      {
+        path: 'planning',
+        loadComponent: () => import('./menbership/planning/planning.page.component').then(c => c.PlanningPageComponent),
+        title: 'Planning',
+      },
+      {
+        path: 'profil',
+        loadComponent: () => import('./menbership/profil/profil.page.component').then(c => c.ProfilPageComponent),
+        title: 'Profil',
+      },
+      {
+        path: 'settings',
+        loadComponent: () => import('./menbership/settings/settings.page.component').then(c => c.SettingsPageComponent),
+        title: 'Settings',
+      },
+      {
+        path: 'workday',
+        loadComponent: () => import('./menbership/workday/workday.page.component').then(c => c.WorkdayPageComponent),
+        title: 'Workday',
+      },
   {
     path: '',
     loadComponent: () => import('./visitor/home/home.page.component').then(c => c.HomePageComponent),
     title: 'Productivity Planner',
   },
+    ]
+  },
+
 ];
--- src/app/menbership/core/shell/shell.layout.component.ts
+++ src/app/menbership/core/shell/shell.layout.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './shell.layout.component.html',
   styleUrl: './shell.layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
