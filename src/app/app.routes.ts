cdd2a314c101cdd36efacd0d62210b6ada3c700d
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-08-10 12:26:52 +0200

    chore(app): add lazy loading route update with menbership-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,53 +1,37 @@
 import { Routes } from '@angular/router';
 import { ShellLayoutComponent } from './menbership/core/shell/shell.layout.component';
 
-
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () =>
+      import('./visitor/home/home.page.component').then(
+        (c) => c.HomePageComponent,
+      ),
+    title: 'Productivity Planner',
+  },
+  {
     path: 'signup',
-    loadComponent: () => import('./visitor/signup/signup.page.component').then(c => c.SignupPageComponent),
+    loadComponent: () =>
+      import('./visitor/signup/signup.page.component').then(
+        (c) => c.SignupPageComponent,
+      ),
     title: 'Sign Up',
   },
   {
     path: 'login',
-    loadComponent: () => import('./visitor/login/login.page.component').then(c => c.LoginPageComponent),
+    loadComponent: () =>
+      import('./visitor/login/login.page.component').then(
+        (c) => c.LoginPageComponent,
+      ),
     title: 'Log In',
   },
   {
     path: 'app',
     component: ShellLayoutComponent,
-    children: [
-      {
-        path: 'dashboard',
-        loadComponent: () => import('./menbership/dashboard/dashboard.page.component').then(c => c.DashboardPageComponent),
-        title: 'Dashboard',
-      },
-      {
-        path: 'planning',
-        loadComponent: () => import('./menbership/planning/planning.page.component').then(c => c.PlanningPageComponent),
-        title: 'Planning',
-      },
-      {
-        path: 'profil',
-        loadComponent: () => import('./menbership/profil/profil.page.component').then(c => c.ProfilPageComponent),
-        title: 'Profil',
-      },
-      {
-        path: 'settings',
-        loadComponent: () => import('./menbership/settings/settings.page.component').then(c => c.SettingsPageComponent),
-        title: 'Settings',
-      },
-      {
-        path: 'workday',
-        loadComponent: () => import('./menbership/workday/workday.page.component').then(c => c.WorkdayPageComponent),
-        title: 'Workday',
-      },
-  {
-    path: '',
-    loadComponent: () => import('./visitor/home/home.page.component').then(c => c.HomePageComponent),
-    title: 'Productivity Planner',
+    loadChildren: () =>
+      import('./menbership/menbership.routes').then(
+        (routes) => routes.membershipRoutes,
+      ),
   },
-    ]
-  },
-
 ];
