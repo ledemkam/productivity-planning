95a199ffdcf7eac30288451c5470510cdc78bfd1
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-08-10 09:03:37 +0200

    src(app): create  component settings-remove setting  on menbership

---
+++ src/app/menbership/settings/settings.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './settings.page.component.html',
+  styleUrl: './settings.page.component.css'
+})
+export class SettingsPageComponent {
+
+}
