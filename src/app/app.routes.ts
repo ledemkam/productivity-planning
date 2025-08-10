ef9209544170337e6ddbd5578c7dc8522193a8ea
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-08-10 10:27:38 +0200

    chore(app): add all route from menbership

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { HomePageComponent } from './visitor/home/home.page.component';
 import { SignupPageComponent } from './visitor/signup/signup.page.component';
 import { LoginPageComponent } from './visitor/login/login.page.component';
 import { DashboardPageComponent } from './menbership/dashboard/dashboard.page.component';
+import { PlanningPageComponent } from './menbership/planning/planning.page.component';
+import { SettingsPageComponent } from './menbership/settings/settings.page.component';
+import { ProfilPageComponent } from './menbership/profil/profil.page.component';
+import { WorkdayPageComponent } from './menbership/workday/workday.page.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +23,26 @@ export const routes: Routes = [
     path: 'app/dashboard',
     component: DashboardPageComponent,
     title: 'Dashboard',
+  },
+   {
+    path: 'app/planning',
+    component: PlanningPageComponent,
+    title: 'Planning',
+  },
+   {
+    path: 'app/profil',
+    component: ProfilPageComponent,
+    title: 'Profil',
+  },
+   {
+    path: 'app/settings',
+    component: SettingsPageComponent,
+    title: 'Settings',
+  },
+   {
+    path: 'app/workday',
+    component: WorkdayPageComponent,
+    title: 'Workday',
   },
   {
     path: '',
