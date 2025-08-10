fbda79db128c8919a15a4810de724671eecbe4e4
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-08-10 08:59:12 +0200

    src(app): create  component planing and workday on menbership

---
+++ src/app/menbership/planning/planning.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './planning.page.component.html',
+  styleUrl: './planning.page.component.css'
+})
+export class PlanningPageComponent {
+
+}
---
+++ src/app/menbership/workday/workday.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './workday.page.component.html',
+  styleUrl: './workday.page.component.css'
+})
+export class WorkdayPageComponent {
+
+}
