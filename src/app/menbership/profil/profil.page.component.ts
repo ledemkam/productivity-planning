30e7a84cb22593d17594879845dcd3fdf1c60085
Eric k. <81742990+ledemkam@users.noreply.github.com>
2025-08-10 09:00:01 +0200

    src(app): create  component profil  on menbership

---
+++ src/app/menbership/profil/profil.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './profil.page.component.html',
+  styleUrl: './profil.page.component.css'
+})
+export class ProfilPageComponent {
+
+}
